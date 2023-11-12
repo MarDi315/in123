bfdab2fec1842574d739d26e98e889977a0ebfee
MarDi315 <148238090+MarDi315@users.noreply.github.com>
2023-11-13 02:20:20 +0300

    Add files via upload

---
+++ inst123.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# URL для использования и логиниться через API сайта Instagram
+url='https://api.instagram.com/v1/accounts/login/'
+
+# Получить токен для имени пользователя
+read -p "Enter the username: " username
+
+# Логиниться в Instagram с помощью API
+response=$(curl -s -X POST $url --data "username=$username")
+
+# Получаем пароль из ответа
+password=$(echo $response | jq -r .password)
+
+# Проверяем статус ответа
+status=$(echo $response | jq -r .status)
+
+# Выводим результат в зависимости от статуса
+if [ "$status" = "ok" ]; then
+    echo "Password: $password"
+else
+    echo "Incorrect username!"
+fi
